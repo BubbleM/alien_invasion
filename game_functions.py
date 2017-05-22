5e3d8d08781cbd7e6473e81cff7364be40eda52e
BubbleM <1870408473@qq.com>
2017-05-22 22:21:36 +0800

    重构

---
+++ game_functions.py
@@ -0,0 +1,24 @@
+# 玩家退出时，将使用模块sys来退出游戏
+import sys
+
+import pygame
+
+def check_events(ship):
+	# 响应键盘和鼠标事件
+	# pygame.event.get() 为访问Pygame检测到的事件
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_RIGHT:
+				# 向右移动飞船
+				ship.rect.centerx += 1
+
+def update_screen(ai_settings,screen,ship):
+	"""更新屏幕上的图像，并切换到新屏幕"""
+	# 每次循环时都重绘屏幕
+	screen.fill(ai_settings.bg_color)
+	ship.blitme()
+	# 让最近绘制的屏幕可见
+	# 每次执行while循环时都绘制一个空屏幕，并擦去旧屏幕，使得只有新屏幕可见
+	pygame.display.flip()
--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ class Settings():
 	def __init__(self):
 		"""初始化游戏的设置"""
 		# 屏幕设置
-		self.screen_width = 1200
-		self.screen_height = 800
+		self.screen_width = 1000
+		self.screen_height = 600
 		self.bg_color = (230,230,230)
